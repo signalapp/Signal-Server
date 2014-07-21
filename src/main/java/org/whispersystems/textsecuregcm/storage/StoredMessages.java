69de9f6684a4f55ede36d098343a43b847a84450
Moxie Marlinspike <moxie@thoughtcrime.org>
2014-07-21 01:20:14 -0700

    Fix stored message retrieval.
    // FREEBIE

--- src/main/java/org/whispersystems/textsecuregcm/storage/StoredMessages.java
+++ src/main/java/org/whispersystems/textsecuregcm/storage/StoredMessages.java
@@ -68,7 +68,7 @@ public class StoredMessages {
       jedis = jedisPool.getResource();
       String message;
 
-      while ((message = jedis.rpop(QUEUE_PREFIX + accountId + ":" + deviceId)) != null) {
+      while ((message = jedis.rpop(getKey(accountId, deviceId))) != null) {
         messages.add(message);
       }
 
