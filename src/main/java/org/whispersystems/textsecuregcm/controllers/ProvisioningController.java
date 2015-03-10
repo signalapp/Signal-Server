7011f3c3c7db7e4c22a1f7cb566cd356540fa06b
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-03-10 16:00:20 -0700

    Fix 500 on validation error

--- src/main/java/org/whispersystems/textsecuregcm/controllers/ProvisioningController.java
+++ src/main/java/org/whispersystems/textsecuregcm/controllers/ProvisioningController.java
@@ -15,6 +15,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -37,6 +38,7 @@ public class ProvisioningController {
   @Path("/{destination}")
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
   public void sendProvisioningMessage(@Auth                     Account source,
                                       @PathParam("destination") String destinationName,
                                       @Valid                    ProvisioningMessage message)
