fd8e8d147545eddc44e8067b389027a68428676d
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-04-16 11:33:16 -0700

    Catch WebApplicationException inside WebsocketConnection.
    // FREEBIE

--- src/main/java/org/whispersystems/textsecuregcm/websocket/WebSocketConnection.java
+++ src/main/java/org/whispersystems/textsecuregcm/websocket/WebSocketConnection.java
@@ -25,6 +25,7 @@ import org.whispersystems.websocket.messages.WebSocketResponseMessage;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.ws.rs.WebApplicationException;
 import java.io.IOException;
 import java.util.List;
 
@@ -136,7 +137,7 @@ public class WebSocketConnection implements DispatchChannel {
       receiptSender.sendReceipt(account, message.getSource(), message.getTimestamp(),
                                 message.hasRelay() ? Optional.of(message.getRelay()) :
                                                      Optional.<String>absent());
-    } catch (IOException | NoSuchUserException | TransientPushFailureException | NotPushRegisteredException e) {
+    } catch (IOException | NoSuchUserException | TransientPushFailureException | NotPushRegisteredException | WebApplicationException e) {
       logger.warn("sendDeliveryReceiptFor", e);
     }
   }
