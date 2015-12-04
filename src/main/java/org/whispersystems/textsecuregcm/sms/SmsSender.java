7a6c8341d43084c895b76494970f86055a2c1e43
Emmanuel Odeke <emm.odeke@gmail.com>
2015-12-04 12:45:42 -0700

    SmsSender: switch string from TextSecure to Signal
    Fixes #45.
    
    Since TextSecure is now Signal, apply the name change to
    Android too.

--- src/main/java/org/whispersystems/textsecuregcm/sms/SmsSender.java
+++ src/main/java/org/whispersystems/textsecuregcm/sms/SmsSender.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 public class SmsSender {
 
   static final String SMS_IOS_VERIFICATION_TEXT = "Your Signal verification code: %s\n\nOr tap: sgnl://verify/%s";
-  static final String SMS_VERIFICATION_TEXT     = "Your TextSecure verification code: %s";
+  static final String SMS_VERIFICATION_TEXT     = "Your Signal verification code: %s";
   static final String VOX_VERIFICATION_TEXT     = "Your Signal verification code is: ";
 
   private final Logger logger = LoggerFactory.getLogger(SmsSender.class);
