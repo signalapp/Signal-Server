3d7e4766f780717319f03ed3443594fe9cd402c6
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-06-06 08:13:29 -0700

    Remove unused push configuration fields
    // FREEBIE

--- src/main/java/org/whispersystems/textsecuregcm/configuration/PushConfiguration.java
+++ src/main/java/org/whispersystems/textsecuregcm/configuration/PushConfiguration.java
@@ -6,41 +6,10 @@ import org.hibernate.validator.constraints.NotEmpty;
 import javax.validation.constraints.Min;
 
 public class PushConfiguration {
-  @JsonProperty
-  @NotEmpty
-  private String host;
-
-  @JsonProperty
-  @Min(1)
-  private int port;
-
-  @JsonProperty
-  @NotEmpty
-  private String username;
-
-  @JsonProperty
-  @NotEmpty
-  private String password;
 
   @JsonProperty
   @Min(0)
   private int queueSize = 200;
-
-  public String getHost() {
-    return host;
-  }
-
-  public int getPort() {
-    return port;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 
   public int getQueueSize() {
     return queueSize;
