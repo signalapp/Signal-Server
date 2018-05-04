3b2eacfc8e4588177296c3e468fa688c887c256a
Moxie Marlinspike <moxie+github@signal.org>
2018-05-04 02:20:29 -0700

    Remove dependency

--- src/main/java/org/whispersystems/textsecuregcm/metrics/LoggingNetworkAppenderFactory.java
+++ src/main/java/org/whispersystems/textsecuregcm/metrics/LoggingNetworkAppenderFactory.java
@@ -1,22 +1,28 @@
 package org.whispersystems.textsecuregcm.metrics;
 
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.Appender;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.papertrailapp.logback.Syslog4jAppender;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.productivity.java.syslog4j.SyslogConfigIF;
+import org.productivity.java.syslog4j.SyslogIF;
+import org.productivity.java.syslog4j.SyslogRuntimeException;
+import org.productivity.java.syslog4j.impl.net.tcp.ssl.SSLTCPNetSyslogConfig;
+
+import javax.validation.constraints.NotNull;
+import java.util.TimeZone;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.util.LevelToSyslogSeverity;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
 import io.dropwizard.logging.AbstractAppenderFactory;
 import io.dropwizard.logging.async.AsyncAppenderFactory;
 import io.dropwizard.logging.filter.LevelFilterFactory;
 import io.dropwizard.logging.layout.LayoutFactory;
 import io.dropwizard.validation.PortRange;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.productivity.java.syslog4j.impl.net.tcp.ssl.SSLTCPNetSyslogConfig;
-
-import javax.validation.constraints.NotNull;
-import java.util.TimeZone;
 
 @JsonTypeName("papertrail")
 public class LoggingNetworkAppenderFactory extends AbstractAppenderFactory<ILoggingEvent> {
@@ -117,5 +123,76 @@ public class LoggingNetworkAppenderFactory extends AbstractAppenderFactory<ILogg
     syslogAppender.start();
 
     return syslogAppender;
+  }
+
+
+  private static class Syslog4jAppender<E> extends AppenderBase<E> {
+    SyslogIF syslog;
+    SyslogConfigIF syslogConfig;
+    Layout<E> layout;
+
+    @Override
+    protected void append(E loggingEvent) {
+      syslog.log(getSeverityForEvent(loggingEvent), layout.doLayout(loggingEvent));
+    }
+
+    @Override
+    public void start() {
+      super.start();
+
+      synchronized (this) {
+        try {
+          Class syslogClass = syslogConfig.getSyslogClass();
+          syslog = (SyslogIF) syslogClass.newInstance();
+
+          syslog.initialize(syslogClass.getSimpleName(), syslogConfig);
+        } catch (ClassCastException | IllegalAccessException | InstantiationException cse) {
+          throw new SyslogRuntimeException(cse);
+        }
+      }
+    }
+
+    @Override
+    public void stop() {
+      super.stop();
+
+      synchronized(this) {
+        if (syslog != null) {
+          syslog.shutdown();
+          syslog = null;
+        }
+      }
+    }
+
+    /**
+     * Convert a level to equivalent syslog severity. Only levels for printing
+     * methods i.e DEBUG, WARN, INFO and ERROR are converted.
+     *
+     * @see ch.qos.logback.core.net.SyslogAppenderBase#getSeverityForEvent(java.lang.Object)
+     */
+    public int getSeverityForEvent(Object eventObject) {
+      if (eventObject instanceof ILoggingEvent) {
+        ILoggingEvent event = (ILoggingEvent) eventObject;
+        return LevelToSyslogSeverity.convert(event);
+      } else {
+        return SyslogIF.LEVEL_INFO;
+      }
+    }
+
+    public SyslogConfigIF getSyslogConfig() {
+      return syslogConfig;
+    }
+
+    public void setSyslogConfig(SyslogConfigIF syslogConfig) {
+      this.syslogConfig = syslogConfig;
+    }
+
+    public Layout<E> getLayout() {
+      return layout;
+    }
+
+    public void setLayout(Layout<E> layout) {
+      this.layout = layout;
+    }
   }
 }
