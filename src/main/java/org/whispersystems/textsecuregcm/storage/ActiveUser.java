a6a42fececcb9d463d3bc8965a03ef08ad6564a3
Brian Acton <acton@signal.org>
2018-11-03 21:23:48 -0700

    add copyright

--- src/main/java/org/whispersystems/textsecuregcm/storage/ActiveUser.java
+++ src/main/java/org/whispersystems/textsecuregcm/storage/ActiveUser.java
@@ -1,4 +1,21 @@
+/**
+ * Copyright (C) 2018 Open WhisperSystems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.whispersystems.textsecuregcm.storage;
+
 public class ActiveUser {
     private long id;
     private long lastActiveMs;
@@ -8,10 +25,10 @@ public class ActiveUser {
     public ActiveUser() { }
 
     public ActiveUser(long id, long lastActiveMs, int deviceId, int platform) {
-      this.id = id;
+      this.id           = id;
       this.lastActiveMs = lastActiveMs;
-      this.deviceId = deviceId;
-      this.platform = platform;
+      this.deviceId     = deviceId;
+      this.platform     = platform;
     }
 
     public long getId()           { return id; }
