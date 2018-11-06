428fe1acc9c2155b0d0b8b2891266950c3dfa0a7
Brian Acton <acton@signal.org>
2018-11-05 19:24:09 -0800

    add test stubs. wip

---
+++ src/test/java/org/whispersystems/textsecuregcm/tests/storage/ActiveUserCounterTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2018 Open WhisperSystems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.whispersystems.textsecuregcm.tests.storage;
+
+import com.google.common.base.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.whispersystems.textsecuregcm.storage.Account;
+import org.whispersystems.textsecuregcm.storage.Accounts;
+import org.whispersystems.textsecuregcm.util.Util;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class ActiveUserCounterTest {
+  private final Account  account  = mock(Account.class);
+  private final Accounts accounts = mock(Accounts.class);
+
+  @Before
+  public void setup() {
+  }
+
+  @Test
+  public void testValid() {
+  }
+
+  @Test
+  public void testInProgress() {
+  }
+  
+  @Test
+  public void testLastChunk() {
+  }
+  
+  @Test
+  public void testNotFound() {
+  }
+  
+}
+
