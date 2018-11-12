55c1ef2e4940c734e46f098e49f891e1d750fc21
Brian Acton <acton@signal.org>
2018-11-11 17:20:26 -0800

    add method verification to tests. wip

--- src/test/java/org/whispersystems/textsecuregcm/tests/storage/ActiveUserCounterTest.java
+++ src/test/java/org/whispersystems/textsecuregcm/tests/storage/ActiveUserCounterTest.java
@@ -44,6 +44,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class ActiveUserCounterTest {
@@ -85,10 +86,10 @@ public class ActiveUserCounterTest {
     when(invalidAccount.getMasterDevice()).thenReturn(Optional.ofNullable(null));
 
     when(accounts.getAllFrom(eq("+"), anyInt())).thenReturn(Arrays.asList(iosAccount, androidAccount, invalidAccount));
-    when(accounts.getAllFrom(eq("+3"), anyInt())).thenReturn(Collections.emptyList());
 
     when(metricsFactory.getReporters()).thenReturn(ImmutableList.of());
 
+    //when(activeUserCache.setLastNumberVisited(any())).thenReturn(Optional.of("+"));
     when(activeUserCache.getLastNumberVisited()).thenReturn(Optional.of("+"));
     when(activeUserCache.getDateToReport(anyInt())).thenReturn(20181101);
     when(activeUserCache.claimActiveWorker(any(), anyLong())).thenReturn(true);
@@ -96,22 +97,200 @@ public class ActiveUserCounterTest {
   }
 
   @Test
-  public void test() {
+  public void testStart() {
 
-    int today = activeUserCounter.getDateOfToday();
-    activeUserCounter.doPeriodicWork(today);
-    /*
+    int     today      = activeUserCounter.getDateOfToday();
+    boolean doMoreWork = activeUserCounter.doPeriodicWork(today);
+
+    assertThat(doMoreWork).isTrue();
+
+    verify(activeUserCache, times(1)).claimActiveWorker(any(), anyLong());
+    verify(activeUserCache, times(1)).getLastNumberVisited();
+    verify(activeUserCache, times(1)).getDateToReport(anyInt());
+
+    verify(activeUserCache).setLastNumberVisited(eq(Optional.of("+")));
+    verify(activeUserCache).setDateToReport(eq(today));
+    verify(activeUserCache, times(1)).setDateToReport(anyInt());
+    verify(activeUserCache, times(1)).resetTallies(any(), any());
+
+    verify(accounts, times(1)).getAllFrom(any(), anyInt());
+
+    verify(iosAccount, times(1)).getNumber();
+    verify(iosAccount, times(1)).getMasterDevice();
+
+    verify(androidAccount, times(1)).getNumber();
+    verify(androidAccount, times(1)).getMasterDevice();
+
+    verify(invalidAccount, times(1)).getNumber();
+    verify(invalidAccount, times(1)).getMasterDevice();
+
+    verify(iosDevice, times(1)).getLastSeen();
+    verify(iosDevice, times(1)).getApnId();
+    verify(iosDevice, times(0)).getGcmId();
+
+    verify(androidDevice, times(1)).getApnId();
+    verify(androidDevice, times(1)).getGcmId();
+    verify(androidDevice, times(1)).getLastSeen();
+
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+    verify(activeUserCache).setLastNumberVisited(eq(Optional.of("+3")));
+    verify(activeUserCache, times(2)).setLastNumberVisited(any());
+
+    verify(activeUserCache, times(0)).getFinalTallies(any(), any());
+    verify(metricsFactory, times(0)).getReporters();
+
+    verify(activeUserCache, times(1)).releaseActiveWorker(any());
+
+    verifyZeroInteractions(metricsFactory);
+
     verifyNoMoreInteractions(iosDevice);
     verifyNoMoreInteractions(androidDevice);
-
     verifyNoMoreInteractions(iosAccount);
     verifyNoMoreInteractions(androidAccount);
     verifyNoMoreInteractions(invalidAccount);
-
     verifyNoMoreInteractions(accounts);
-    verifyNoMoreInteractions(metricsFactory);
     verifyNoMoreInteractions(activeUserCache);
-    */
-
   }
+
+  @Test
+  public void testInProgressValid() {
+
+    int today = activeUserCounter.getDateOfToday();
+
+    when(accounts.getAllFrom(eq("+"), anyInt())).thenReturn(Arrays.asList(iosAccount));
+    when(activeUserCache.getDateToReport(anyInt())).thenReturn(today);
+
+    boolean doMoreWork = activeUserCounter.doPeriodicWork(today);
+
+    assertThat(doMoreWork).isTrue();
+
+    verify(activeUserCache, times(1)).claimActiveWorker(any(), anyLong());
+    verify(activeUserCache, times(1)).getLastNumberVisited();
+    verify(activeUserCache, times(1)).getDateToReport(anyInt());
+
+    verify(activeUserCache, times(0)).setDateToReport(anyInt());
+    verify(activeUserCache, times(0)).resetTallies(any(), any());
+
+    //ProcessChunk
+    verify(accounts, times(1)).getAllFrom(any(), anyInt());
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+
+    verify(iosAccount, times(1)).getNumber();
+    verify(iosAccount, times(1)).getMasterDevice();
+
+    verify(iosDevice, times(1)).getLastSeen();
+    verify(iosDevice, times(1)).getApnId();
+    verify(iosDevice, times(0)).getGcmId();
+
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+    verify(activeUserCache).setLastNumberVisited(eq(Optional.of("+1")));
+    verify(activeUserCache, times(1)).setLastNumberVisited(any());
+
+
+    //registerMetrics
+    verify(activeUserCache, times(0)).getFinalTallies(any(), any());
+    verify(metricsFactory, times(0)).getReporters();
+
+    verify(activeUserCache, times(1)).releaseActiveWorker(any());
+
+    verifyZeroInteractions(androidDevice);
+    verifyZeroInteractions(androidAccount);
+    verifyZeroInteractions(invalidAccount);
+
+    verifyNoMoreInteractions(iosDevice);
+    verifyNoMoreInteractions(iosAccount);
+    verifyNoMoreInteractions(metricsFactory);
+    verifyNoMoreInteractions(accounts);
+    verifyNoMoreInteractions(activeUserCache);
+  }
+
+    @Test
+  public void testInProgressInvalid() {
+
+    int today = activeUserCounter.getDateOfToday();
+
+    when(accounts.getAllFrom(eq("+"), anyInt())).thenReturn(Arrays.asList(invalidAccount));
+    when(activeUserCache.getDateToReport(anyInt())).thenReturn(today);
+
+    boolean doMoreWork = activeUserCounter.doPeriodicWork(today);
+
+    assertThat(doMoreWork).isTrue();
+
+    verify(activeUserCache, times(1)).claimActiveWorker(any(), anyLong());
+    verify(activeUserCache, times(1)).getLastNumberVisited();
+    verify(activeUserCache, times(1)).getDateToReport(anyInt());
+
+    verify(activeUserCache, times(0)).setDateToReport(anyInt());
+    verify(activeUserCache, times(0)).resetTallies(any(), any());
+
+    //ProcessChunk
+    verify(accounts, times(1)).getAllFrom(any(), anyInt());
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+
+    verify(invalidAccount, times(1)).getNumber();
+    verify(invalidAccount, times(1)).getMasterDevice();
+
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+    verify(activeUserCache).setLastNumberVisited(eq(Optional.of("+3")));
+    verify(activeUserCache, times(1)).setLastNumberVisited(any());
+
+
+    //registerMetrics
+    verify(activeUserCache, times(0)).getFinalTallies(any(), any());
+    verify(metricsFactory, times(0)).getReporters();
+
+    verify(activeUserCache, times(1)).releaseActiveWorker(any());
+
+    verifyZeroInteractions(iosDevice);
+    verifyZeroInteractions(iosAccount);
+    verifyZeroInteractions(androidDevice);
+    verifyZeroInteractions(androidAccount);
+
+    verifyNoMoreInteractions(invalidAccount);
+    verifyNoMoreInteractions(metricsFactory);
+    verifyNoMoreInteractions(accounts);
+    verifyNoMoreInteractions(activeUserCache);
+  }
+
+  @Test
+  public void testFinish() {
+
+    int today = activeUserCounter.getDateOfToday();
+
+    when(accounts.getAllFrom(eq("+"), anyInt())).thenReturn(Collections.emptyList());
+    when(activeUserCache.getDateToReport(anyInt())).thenReturn(today);
+
+    boolean doMoreWork = activeUserCounter.doPeriodicWork(today);
+
+    assertThat(doMoreWork).isFalse();
+
+    verify(activeUserCache, times(1)).claimActiveWorker(any(), anyLong());
+    verify(activeUserCache, times(1)).getLastNumberVisited();
+    verify(activeUserCache, times(1)).getDateToReport(anyInt());
+
+    verify(activeUserCache, times(0)).setDateToReport(anyInt());
+    verify(activeUserCache, times(0)).resetTallies(any(), any());
+
+    //ProcessChunk
+    verify(accounts, times(1)).getAllFrom(any(), anyInt());
+    verify(activeUserCache,times(2)).incrementTallies(any(), any(), any());
+    verify(activeUserCache).setLastNumberVisited(eq(Optional.ofNullable(null)));
+    verify(activeUserCache, times(1)).setLastNumberVisited(any());
+
+    //registerMetrics
+    verify(activeUserCache, times(2)).getFinalTallies(any(), any());
+    verify(metricsFactory, times(1)).getReporters();
+
+    verify(activeUserCache, times(1)).releaseActiveWorker(any());
+
+    verifyZeroInteractions(iosDevice);
+    verifyZeroInteractions(androidDevice);
+    verifyZeroInteractions(iosAccount);
+    verifyZeroInteractions(androidAccount);
+    verifyZeroInteractions(invalidAccount);
+
+    verifyNoMoreInteractions(metricsFactory);
+    verifyNoMoreInteractions(accounts);
+    verifyNoMoreInteractions(activeUserCache);}
+
 }
