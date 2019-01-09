ea3864549330c80ec90bc0c41ee91806f3190d10
Moxie Marlinspike <moxie+github@signal.org>
2019-01-09 13:56:02 -0800

    Add api endpoint for deleting deprecated signaling key

--- src/main/java/org/whispersystems/textsecuregcm/controllers/AccountController.java
+++ src/main/java/org/whispersystems/textsecuregcm/controllers/AccountController.java
@@ -357,6 +357,14 @@ public class AccountController {
   }
 
   @Timed
+  @DELETE
+  @Path("/signaling_key")
+  public void removeSignalingKey(@Auth Account account) {
+    account.getAuthenticatedDevice().get().setSignalingKey(null);
+    accounts.update(account);
+  }
+
+  @Timed
   @PUT
   @Path("/attributes/")
   @Consumes(MediaType.APPLICATION_JSON)
