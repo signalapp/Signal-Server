3de3fc00ce9827530a9a0bc1181e5ceee279deb3
Moxie Marlinspike <moxie+github@signal.org>
2019-03-28 17:49:41 -0700

    Delete old hystrix stuff

--- src/main/java/org/whispersystems/textsecuregcm/hystrix/GroupKeys.java
+++
@@ -1,9 +0,0 @@
-package org.whispersystems.textsecuregcm.hystrix;
-
-public class GroupKeys {
-
-  public static final String REDIS_CACHE       = "redis_cache";
-  public static final String DATABASE_ACCOUNTS = "database_accounts";
-  public static final String DIRECTORY_SERVICE = "directory_service";
-
-}
