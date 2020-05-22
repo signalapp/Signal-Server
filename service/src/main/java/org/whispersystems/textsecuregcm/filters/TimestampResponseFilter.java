aa10f63d9fe4842a556d0ce478fffc5f198e9a4d
Jon Chambers <jon@signal.org>
2020-05-22 17:24:22 -0400

    Add the timestamp using the `add` method.

--- service/src/main/java/org/whispersystems/textsecuregcm/filters/TimestampResponseFilter.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/filters/TimestampResponseFilter.java
@@ -5,7 +5,6 @@ import org.whispersystems.textsecuregcm.util.TimestampHeaderUtil;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import java.util.Collections;
 
 /**
  * Injects a timestamp header into all outbound responses.
@@ -14,6 +13,6 @@ public class TimestampResponseFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) {
-        responseContext.getStringHeaders().put(TimestampHeaderUtil.TIMESTAMP_HEADER, Collections.singletonList(String.valueOf(System.currentTimeMillis())));
+        responseContext.getHeaders().add(TimestampHeaderUtil.TIMESTAMP_HEADER, System.currentTimeMillis());
     }
 }
