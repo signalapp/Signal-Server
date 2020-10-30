436de3a6a5daa4cbc8b162ec974fed0dc71fd781
Anastasia Kuznetsova <anastasia.kuznetsova@jetbrains.com>
2020-10-30 10:30:45 +0100

    Removed controllers and sqs

--- service/src/main/java/org/whispersystems/textsecuregcm/controllers/InvalidDestinationException.java
+++
@@ -1,7 +0,0 @@
-package org.whispersystems.textsecuregcm.controllers;
-
-public class InvalidDestinationException extends Exception {
-  public InvalidDestinationException(String message) {
-    super(message);
-  }
-}
