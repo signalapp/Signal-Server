adbc4e9fec5a7ebccb9c6d4243c185e1e18ab7e8
Jon Chambers <jon@signal.org>
2020-11-09 18:16:36 -0500

    Record the platforms of clients that send a keepalive without a local presence.

--- service/src/main/java/org/whispersystems/textsecuregcm/controllers/KeepAliveController.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/controllers/KeepAliveController.java
@@ -7,16 +7,21 @@ package org.whispersystems.textsecuregcm.controllers;
 
 import com.codahale.metrics.annotation.Timed;
 import io.dropwizard.auth.Auth;
+import io.micrometer.core.instrument.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.whispersystems.textsecuregcm.push.ClientPresenceManager;
 import org.whispersystems.textsecuregcm.storage.Account;
+import org.whispersystems.textsecuregcm.util.ua.UnrecognizedUserAgentException;
+import org.whispersystems.textsecuregcm.util.ua.UserAgentUtil;
 import org.whispersystems.websocket.session.WebSocketSession;
 import org.whispersystems.websocket.session.WebSocketSessionContext;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import static com.codahale.metrics.MetricRegistry.name;
 
 
 @Path("/v1/keepalive")
@@ -25,6 +30,9 @@ public class KeepAliveController {
   private final Logger logger = LoggerFactory.getLogger(KeepAliveController.class);
 
   private final ClientPresenceManager clientPresenceManager;
+
+  private static final String NO_LOCAL_SUBSCRIPTION_COUNTER_NAME      = name(KeepAliveController.class, "noLocalSubscription");
+  private static final String NO_LOCAL_SUBSCRIPTION_PLATFORM_TAG_NAME = "platform";
 
   public KeepAliveController(final ClientPresenceManager clientPresenceManager) {
     this.clientPresenceManager = clientPresenceManager;
@@ -39,6 +47,16 @@ public class KeepAliveController {
       if (!clientPresenceManager.isLocallyPresent(account.getUuid(), account.getAuthenticatedDevice().get().getId())) {
         logger.warn("***** No local subscription found for {}::{}", account.getUuid(), account.getAuthenticatedDevice().get().getId());
         context.getClient().close(1000, "OK");
+
+        String platform;
+
+        try {
+          platform = UserAgentUtil.parseUserAgentString(context.getClient().getUserAgent()).getPlatform().name().toLowerCase();
+        } catch (UnrecognizedUserAgentException e) {
+          platform = "unknown";
+        }
+
+        Metrics.counter(NO_LOCAL_SUBSCRIPTION_COUNTER_NAME, NO_LOCAL_SUBSCRIPTION_PLATFORM_TAG_NAME, platform).increment();
       }
     }
 
