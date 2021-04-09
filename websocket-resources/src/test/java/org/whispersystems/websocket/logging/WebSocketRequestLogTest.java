fce428ae57cfa0dd0c481d895c314447af385cd8
yoram.halberstam <yoramh@riscogroup.com>
2021-04-09 15:31:15 +0300

    removed temporary comment during merging

--- websocket-resources/src/test/java/org/whispersystems/websocket/logging/WebSocketRequestLogTest.java
+++ websocket-resources/src/test/java/org/whispersystems/websocket/logging/WebSocketRequestLogTest.java
@@ -46,7 +46,6 @@ public class WebSocketRequestLogTest {
     assertThat(listAppender.list.size()).isEqualTo(1);
 
     String loggedLine = new String(listAppender.outputStream.toByteArray());
-    //assertThat(loggedLine).matches("123\\.456\\.789\\.123 \\- \\- \\[[0-9]{2}\\/[a-zA-Z]{3}\\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} (\\-|\\+)[0-9]{4}\\] \"GET \\/v1\\/test WS\" 200 \\- \"\\-\" \"\\-\"\n");
     assertThat(loggedLine).matches("123\\.456\\.789\\.123 \\- \\- \\[[0-9]{2}\\/[a-zA-Z]{3}\\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} [\\-+][0-9]{4}\\] \"GET \\/v1\\/test WS\" 200 \\- \"\\-\" \"\\-\"\n");
   }
 
