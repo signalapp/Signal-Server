d5b0d99a544594001f94a1fd6b4644a341ff9111
Chris Eager <chris@signal.org>
2021-07-01 12:26:31 -0500

    Remove unused method

--- service/src/main/java/org/whispersystems/textsecuregcm/util/AttributeValues.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/util/AttributeValues.java
@@ -43,10 +43,6 @@ public class AttributeValues {
     return AttributeValue.builder().b(value).build();
   }
 
-  public static AttributeValue fromBoolean(boolean value) {
-    return AttributeValue.builder().bool(value).build();
-  }
-
   private static int toInt(AttributeValue av) {
     return Integer.parseInt(av.n());
   }
