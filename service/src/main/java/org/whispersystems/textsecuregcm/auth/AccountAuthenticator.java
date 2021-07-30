4e2284b83fc82c6b67100be56a309c3a62f2574a
Jon Chambers <jon@signal.org>
2021-07-30 12:09:08 -0400

    Retire old GV2 adoption metrics.

--- service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
@@ -4,21 +4,13 @@
  */
 package org.whispersystems.textsecuregcm.auth;
 
-import io.micrometer.core.instrument.Metrics;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
+import java.util.Optional;
 import org.whispersystems.textsecuregcm.storage.Account;
 import org.whispersystems.textsecuregcm.storage.AccountsManager;
 
-import java.util.Optional;
-
-import io.dropwizard.auth.Authenticator;
-import io.dropwizard.auth.basic.BasicCredentials;
-
-import static com.codahale.metrics.MetricRegistry.name;
-
 public class AccountAuthenticator extends BaseAccountAuthenticator implements Authenticator<BasicCredentials, Account> {
-
-  private static final String AUTHENTICATION_COUNTER_NAME     = name(AccountAuthenticator.class, "authenticate");
-  private static final String GV2_CAPABLE_TAG_NAME            = "gv1Migration";
 
   public AccountAuthenticator(AccountsManager accountsManager) {
     super(accountsManager);
@@ -26,14 +18,7 @@ public class AccountAuthenticator extends BaseAccountAuthenticator implements Au
 
   @Override
   public Optional<Account> authenticate(BasicCredentials basicCredentials) {
-    final Optional<Account> maybeAccount = super.authenticate(basicCredentials, true);
-
-    // TODO Remove this temporary counter when we can replace it with more generic feature adoption system
-    maybeAccount.ifPresent(account -> {
-      Metrics.counter(AUTHENTICATION_COUNTER_NAME, GV2_CAPABLE_TAG_NAME, String.valueOf(account.isGv1MigrationSupported())).increment();
-    });
-
-    return maybeAccount;
+    return super.authenticate(basicCredentials, true);
   }
 
 }
