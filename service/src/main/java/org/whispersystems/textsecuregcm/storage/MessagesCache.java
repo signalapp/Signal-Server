3e8d79e147e8cc92dfd790d39a3b74f59820dbe4
Ehren Kret <ehren@signal.org>
2021-08-11 17:38:55 -0500

    Remove unused lua script to delete by sender and timestamp

--- service/src/main/java/org/whispersystems/textsecuregcm/storage/MessagesCache.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/storage/MessagesCache.java
@@ -54,7 +54,6 @@ public class MessagesCache extends RedisClusterPubSubAdapter<String, String> imp
     private final ExecutorService notificationExecutorService;
 
     private final ClusterLuaScript insertScript;
-    private final ClusterLuaScript removeBySenderScript;
     private final ClusterLuaScript removeByGuidScript;
     private final ClusterLuaScript getItemsScript;
     private final ClusterLuaScript removeQueueScript;
@@ -99,7 +98,6 @@ public class MessagesCache extends RedisClusterPubSubAdapter<String, String> imp
         this.notificationExecutorService = notificationExecutorService;
 
         this.insertScript             = ClusterLuaScript.fromResource(insertCluster, "lua/insert_item.lua",           ScriptOutputType.INTEGER);
-        this.removeBySenderScript     = ClusterLuaScript.fromResource(readDeleteCluster, "lua/remove_item_by_sender.lua", ScriptOutputType.VALUE);
         this.removeByGuidScript       = ClusterLuaScript.fromResource(readDeleteCluster, "lua/remove_item_by_guid.lua",   ScriptOutputType.MULTI);
         this.getItemsScript           = ClusterLuaScript.fromResource(readDeleteCluster, "lua/get_items.lua",             ScriptOutputType.MULTI);
         this.removeQueueScript        = ClusterLuaScript.fromResource(readDeleteCluster, "lua/remove_queue.lua",          ScriptOutputType.STATUS);
