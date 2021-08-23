739c5bf22c6ba4a50a444a195ba8261f48c8ff68
Jon Chambers <jon@signal.org>
2021-08-23 17:09:10 -0400

    Add a counter to estimate announcement group adoption

--- service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
@@ -7,10 +7,15 @@ package org.whispersystems.textsecuregcm.auth;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 import java.util.Optional;
+import io.micrometer.core.instrument.Metrics;
 import org.whispersystems.textsecuregcm.storage.AccountsManager;
+
+import static com.codahale.metrics.MetricRegistry.name;
 
 public class AccountAuthenticator extends BaseAccountAuthenticator implements
     Authenticator<BasicCredentials, AuthenticatedAccount> {
+
+  private static final String AUTHENTICATION_COUNTER_NAME = name(AccountAuthenticator.class, "authenticate");
 
   public AccountAuthenticator(AccountsManager accountsManager) {
     super(accountsManager);
@@ -18,7 +23,16 @@ public class AccountAuthenticator extends BaseAccountAuthenticator implements
 
   @Override
   public Optional<AuthenticatedAccount> authenticate(BasicCredentials basicCredentials) {
-    return super.authenticate(basicCredentials, true);
+    final Optional<AuthenticatedAccount> maybeAuthenticatedAccount = super.authenticate(basicCredentials, true);
+
+    // TODO Remove after announcement groups have launched
+    maybeAuthenticatedAccount.ifPresent(authenticatedAccount ->
+        Metrics.counter(AUTHENTICATION_COUNTER_NAME,
+            "supportsAnnouncementGroups",
+            String.valueOf(authenticatedAccount.getAccount().isAnnouncementGroupSupported()))
+            .increment());
+
+    return maybeAuthenticatedAccount;
   }
 
 }
