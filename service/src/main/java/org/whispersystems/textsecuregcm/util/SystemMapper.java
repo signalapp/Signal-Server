75661fa8006fbcf2f283696c0022885ee657b9fc
Ehren Kret <ehren@signal.org>
2021-09-20 14:15:16 -0500

    Add JavaTimeModule

--- service/src/main/java/org/whispersystems/textsecuregcm/util/SystemMapper.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/util/SystemMapper.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class SystemMapper {
 
@@ -17,6 +18,7 @@ public class SystemMapper {
   static {
     mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
     mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    mapper.registerModule(new JavaTimeModule());
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
