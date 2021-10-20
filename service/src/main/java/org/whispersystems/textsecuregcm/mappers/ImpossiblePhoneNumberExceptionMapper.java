11598e855fea45fd2bad2b46e2b73ae63d78daa0
Jon Chambers <jon@signal.org>
2021-10-20 16:15:56 -0400

    Count non-normalized or impossible numbers

--- service/src/main/java/org/whispersystems/textsecuregcm/mappers/ImpossiblePhoneNumberExceptionMapper.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/mappers/ImpossiblePhoneNumberExceptionMapper.java
@@ -5,14 +5,23 @@
 
 package org.whispersystems.textsecuregcm.mappers;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.whispersystems.textsecuregcm.util.ImpossiblePhoneNumberException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
+import static org.whispersystems.textsecuregcm.metrics.MetricsUtil.name;
+
 public class ImpossiblePhoneNumberExceptionMapper implements ExceptionMapper<ImpossiblePhoneNumberException> {
+
+  private static final Counter IMPOSSIBLE_NUMBER_COUNTER =
+      Metrics.counter(name(ImpossiblePhoneNumberExceptionMapper.class, "impossibleNumbers"));
 
   @Override
   public Response toResponse(final ImpossiblePhoneNumberException exception) {
+    IMPOSSIBLE_NUMBER_COUNTER.increment();
+
     return Response.status(Response.Status.BAD_REQUEST).build();
   }
 }
--- service/src/main/java/org/whispersystems/textsecuregcm/mappers/NonNormalizedPhoneNumberExceptionMapper.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/mappers/NonNormalizedPhoneNumberExceptionMapper.java
@@ -5,15 +5,24 @@
 
 package org.whispersystems.textsecuregcm.mappers;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.whispersystems.textsecuregcm.util.NonNormalizedPhoneNumberException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
+import static org.whispersystems.textsecuregcm.metrics.MetricsUtil.name;
+
 public class NonNormalizedPhoneNumberExceptionMapper implements ExceptionMapper<NonNormalizedPhoneNumberException> {
+
+  private static final Counter NON_NORMALIZED_NUMBER_COUNTER =
+      Metrics.counter(name(NonNormalizedPhoneNumberExceptionMapper.class, "nonNormalizedNumbers"));
 
   @Override
   public Response toResponse(final NonNormalizedPhoneNumberException exception) {
+    NON_NORMALIZED_NUMBER_COUNTER.increment();
+
     return Response.status(Status.BAD_REQUEST)
         .entity(new NonNormalizedPhoneNumberResponse(exception.getOriginalNumber(), exception.getNormalizedNumber()))
         .build();
