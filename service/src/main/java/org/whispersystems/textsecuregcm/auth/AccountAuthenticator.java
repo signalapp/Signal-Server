b608ece57e4a912e2d5a1a941466d688a268a402
Chris Eager <chris@signal.org>
2022-03-11 12:47:37 -0800

    Remove `supportsAnnouncementGroups` metric

--- service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/auth/AccountAuthenticator.java
@@ -7,15 +7,10 @@ package org.whispersystems.textsecuregcm.auth;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 import java.util.Optional;
-import io.micrometer.core.instrument.Metrics;
 import org.whispersystems.textsecuregcm.storage.AccountsManager;
-
-import static com.codahale.metrics.MetricRegistry.name;
 
 public class AccountAuthenticator extends BaseAccountAuthenticator implements
     Authenticator<BasicCredentials, AuthenticatedAccount> {
-
-  private static final String AUTHENTICATION_COUNTER_NAME = name(AccountAuthenticator.class, "authenticate");
 
   public AccountAuthenticator(AccountsManager accountsManager) {
     super(accountsManager);
@@ -23,16 +18,7 @@ public class AccountAuthenticator extends BaseAccountAuthenticator implements
 
   @Override
   public Optional<AuthenticatedAccount> authenticate(BasicCredentials basicCredentials) {
-    final Optional<AuthenticatedAccount> maybeAuthenticatedAccount = super.authenticate(basicCredentials, true);
-
-    // TODO Remove after announcement groups have launched
-    maybeAuthenticatedAccount.ifPresent(authenticatedAccount ->
-        Metrics.counter(AUTHENTICATION_COUNTER_NAME,
-            "supportsAnnouncementGroups",
-            String.valueOf(authenticatedAccount.getAccount().isAnnouncementGroupSupported()))
-            .increment());
-
-    return maybeAuthenticatedAccount;
+    return super.authenticate(basicCredentials, true);
   }
 
 }
