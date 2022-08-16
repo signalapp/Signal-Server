50f5d760c91c04dcd2e92b4b91882380454c94f1
Jon Chambers <jon@signal.org>
2022-08-16 10:37:01 -0400

    Use existing tagging tools for keepalive counters

--- service/src/main/java/org/whispersystems/textsecuregcm/controllers/KeepAliveController.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/controllers/KeepAliveController.java
@@ -13,9 +13,11 @@ import io.micrometer.core.instrument.Metrics;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import io.micrometer.core.instrument.Tags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.whispersystems.textsecuregcm.auth.AuthenticatedAccount;
+import org.whispersystems.textsecuregcm.metrics.UserAgentTagUtil;
 import org.whispersystems.textsecuregcm.push.ClientPresenceManager;
 import org.whispersystems.textsecuregcm.util.ua.UnrecognizedUserAgentException;
 import org.whispersystems.textsecuregcm.util.ua.UserAgentUtil;
@@ -30,8 +32,7 @@ public class KeepAliveController {
 
   private final ClientPresenceManager clientPresenceManager;
 
-  private static final String NO_LOCAL_SUBSCRIPTION_COUNTER_NAME      = name(KeepAliveController.class, "noLocalSubscription");
-  private static final String NO_LOCAL_SUBSCRIPTION_PLATFORM_TAG_NAME = "platform";
+  private static final String NO_LOCAL_SUBSCRIPTION_COUNTER_NAME = name(KeepAliveController.class, "noLocalSubscription");
 
   public KeepAliveController(final ClientPresenceManager clientPresenceManager) {
     this.clientPresenceManager = clientPresenceManager;
@@ -50,15 +51,9 @@ public class KeepAliveController {
 
         context.getClient().close(1000, "OK");
 
-        String platform;
-
-        try {
-          platform = UserAgentUtil.parseUserAgentString(context.getClient().getUserAgent()).getPlatform().name().toLowerCase();
-        } catch (UnrecognizedUserAgentException e) {
-          platform = "unknown";
-        }
-
-        Metrics.counter(NO_LOCAL_SUBSCRIPTION_COUNTER_NAME, NO_LOCAL_SUBSCRIPTION_PLATFORM_TAG_NAME, platform).increment();
+        Metrics.counter(NO_LOCAL_SUBSCRIPTION_COUNTER_NAME,
+            Tags.of(UserAgentTagUtil.getPlatformTag(context.getClient().getUserAgent())))
+            .increment();
       }
     }
 
