2683f1c6e7b79269a79da0bb4a253f00d0e07c4b
Katherine Yen <katherine@signal.org>
2023-03-13 15:35:27 -0700

    Encode username hash to base64 string without padding

--- service/src/main/java/org/whispersystems/textsecuregcm/entities/AccountIdentityResponse.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/entities/AccountIdentityResponse.java
@@ -5,12 +5,17 @@
 
 package org.whispersystems.textsecuregcm.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.whispersystems.textsecuregcm.util.ByteArrayBase64UrlAdapter;
 import java.util.UUID;
 import javax.annotation.Nullable;
 
 public record AccountIdentityResponse(UUID uuid,
                                       String number,
                                       UUID pni,
+                                      @JsonSerialize(using = ByteArrayBase64UrlAdapter.Serializing.class)
+                                      @JsonDeserialize(using = ByteArrayBase64UrlAdapter.Deserializing.class)
                                       @Nullable byte[] usernameHash,
                                       boolean storageCapable) {
 }
