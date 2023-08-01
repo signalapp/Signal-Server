8b1382694999477fc0661bcbc5b44a6c9000a799
Jon Chambers <jon@signal.org>
2023-08-01 17:20:17 -0400

    Convert `DeviceInfo` and `DeviceInfoList` to a record

--- service/src/main/java/org/whispersystems/textsecuregcm/entities/DeviceInfoList.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/entities/DeviceInfoList.java
@@ -5,16 +5,7 @@
 
 package org.whispersystems.textsecuregcm.entities;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 
-public class DeviceInfoList {
-
-  @JsonProperty
-  private List<DeviceInfo> devices;
-
-  public DeviceInfoList(List<DeviceInfo> devices) {
-    this.devices = devices;
-  }
+public record DeviceInfoList(List<DeviceInfo> devices) {
 }
