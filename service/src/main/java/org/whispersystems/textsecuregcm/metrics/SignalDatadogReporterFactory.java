7f37c8ee5e68f30af717901ae262eb62dd41087a
Jon Chambers <jon@signal.org>
2023-08-17 16:04:51 -0400

    Retire now-unused HTTP transport configuration for Datadog metric reporter

--- service/src/main/java/org/whispersystems/textsecuregcm/metrics/SignalDatadogReporterFactory.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/metrics/SignalDatadogReporterFactory.java
@@ -15,11 +15,9 @@ import com.codahale.metrics.ScheduledReporter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.dropwizard.metrics.BaseReporterFactory;
-import io.dropwizard.util.Duration;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Optional;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -28,11 +26,8 @@ import org.coursera.metrics.datadog.DatadogReporter.Expansion;
 import org.coursera.metrics.datadog.DefaultMetricNameFormatterFactory;
 import org.coursera.metrics.datadog.DynamicTagsCallbackFactory;
 import org.coursera.metrics.datadog.MetricNameFormatterFactory;
-import org.coursera.metrics.datadog.transport.HttpTransport;
-import org.coursera.metrics.datadog.transport.Transport;
 import org.coursera.metrics.datadog.transport.UdpTransport;
 import org.whispersystems.textsecuregcm.WhisperServerVersion;
-import org.whispersystems.textsecuregcm.configuration.secrets.SecretString;
 import org.whispersystems.textsecuregcm.util.HostnameUtil;
 
 @JsonTypeName("signal-datadog")
@@ -52,11 +47,6 @@ public class SignalDatadogReporterFactory extends BaseReporterFactory {
   @NotNull
   @JsonProperty
   private MetricNameFormatterFactory metricNameFormatter = new DefaultMetricNameFormatterFactory();
-
-  @Valid
-  @NotNull
-  @JsonProperty("transport")
-  private HttpTransportConfig httpTransportConfig;
 
   @Valid
   @NotNull
@@ -89,12 +79,8 @@ public class SignalDatadogReporterFactory extends BaseReporterFactory {
       }
     }
 
-    final boolean useUdpTransport = Optional.ofNullable(System.getenv("SIGNAL_USE_DATADOG_UDP_TRANSPORT")).isPresent();
-    final Transport transport =
-        useUdpTransport ? udpTransportConfig.udpTransport() : httpTransportConfig.httpTransport();
-
     return DatadogReporter.forRegistry(registry)
-        .withTransport(transport)
+        .withTransport(udpTransportConfig.udpTransport())
         .withHost(HostnameUtil.getLocalHostname())
         .withTags(tagsWithVersion)
         .withPrefix(prefix)
@@ -105,28 +91,6 @@ public class SignalDatadogReporterFactory extends BaseReporterFactory {
         .convertDurationsTo(getDurationUnit())
         .convertRatesTo(getRateUnit())
         .build();
-  }
-
-  public static class HttpTransportConfig {
-
-    @JsonProperty
-    @NotNull
-    private SecretString apiKey;
-
-    @JsonProperty
-    private Duration connectTimeout = Duration.seconds(5);
-
-    @JsonProperty
-    private Duration socketTimeout = Duration.seconds(5);
-
-
-    public HttpTransport httpTransport() {
-      return new HttpTransport.Builder()
-          .withApiKey(apiKey.value())
-          .withConnectTimeout((int) connectTimeout.toMilliseconds())
-          .withSocketTimeout((int) socketTimeout.toMilliseconds())
-          .build();
-    }
   }
 
   public record UdpTransportConfig(@NotNull String statsdHost, @Min(1) int port) {
