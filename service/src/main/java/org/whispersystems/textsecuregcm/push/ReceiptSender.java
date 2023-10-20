cf2353bcf97539d48c3f7f17844eb0bfb524bb17
Chris Eager <chris@signal.org>
2023-10-20 12:43:55 -0500

    Remove InstrumentedExecutorService wrapping

--- service/src/main/java/org/whispersystems/textsecuregcm/push/ReceiptSender.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/push/ReceiptSender.java
@@ -5,8 +5,6 @@
 
 package org.whispersystems.textsecuregcm.push;
 
-import com.codahale.metrics.InstrumentedExecutorService;
-import com.codahale.metrics.SharedMetricRegistries;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
 import java.util.concurrent.ExecutorService;
@@ -18,7 +16,6 @@ import org.whispersystems.textsecuregcm.identity.ServiceIdentifier;
 import org.whispersystems.textsecuregcm.metrics.MetricsUtil;
 import org.whispersystems.textsecuregcm.storage.AccountsManager;
 import org.whispersystems.textsecuregcm.storage.Device;
-import org.whispersystems.textsecuregcm.util.Constants;
 
 public class ReceiptSender {
 
@@ -33,11 +30,7 @@ public class ReceiptSender {
     this.accountManager = accountManager;
     this.messageSender = messageSender;
     this.executor = ExecutorServiceMetrics.monitor(
-            Metrics.globalRegistry,
-            new InstrumentedExecutorService(executor,
-                    SharedMetricRegistries.getOrCreate(Constants.METRICS_NAME),
-                    MetricsUtil.name(ReceiptSender.class, "executor")),
-            MetricsUtil.name(ReceiptSender.class, "executor"), MetricsUtil.PREFIX)
+        Metrics.globalRegistry, executor, MetricsUtil.name(ReceiptSender.class, "executor"), MetricsUtil.PREFIX)
     ;
   }
 
