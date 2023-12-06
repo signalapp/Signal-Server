664f9f36e10bc053fdbcc7ba252735bebb45e57e
Jon Chambers <jon@signal.org>
2023-12-06 17:15:22 -0500

    Use padded base64 encoding for outbound device names

--- service/src/main/java/org/whispersystems/textsecuregcm/entities/DeviceInfo.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/entities/DeviceInfo.java
@@ -7,12 +7,12 @@ package org.whispersystems.textsecuregcm.entities;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.whispersystems.textsecuregcm.util.ByteArrayAdapter;
+import org.whispersystems.textsecuregcm.util.ByteArrayBase64WithPaddingAdapter;
 
 public record DeviceInfo(long id,
 
-                         @JsonSerialize(using = ByteArrayAdapter.Serializing.class)
-                         @JsonDeserialize(using = ByteArrayAdapter.Deserializing.class)
+                         @JsonSerialize(using = ByteArrayBase64WithPaddingAdapter.Serializing.class)
+                         @JsonDeserialize(using = ByteArrayBase64WithPaddingAdapter.Deserializing.class)
                          byte[] name,
 
                          long lastSeen,
