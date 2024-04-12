1df824db7c37f26c6c114e642c22183e53ba9386
Chris Eager <chris@signal.org>
2024-04-12 15:09:33 -0500

    Remap some JsonMappingExceptions to 408 and 400

--- service/src/main/java/org/whispersystems/textsecuregcm/mappers/JsonMappingExceptionMapper.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/mappers/JsonMappingExceptionMapper.java
@@ -3,10 +3,18 @@ package org.whispersystems.textsecuregcm.mappers;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import java.util.concurrent.TimeoutException;
 
 public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
   @Override
   public Response toResponse(final JsonMappingException exception) {
+    if (exception.getCause() instanceof TimeoutException) {
+      return Response.status(Response.Status.REQUEST_TIMEOUT).build();
+    }
+    if ("Early EOF".equals(exception.getMessage())) {
+      // Some sort of timeout or broken connection
+      return Response.status(Response.Status.BAD_REQUEST).build();
+    }
     return Response.status(422).build();
   }
 }
