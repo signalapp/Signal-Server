cf307db31da901ac65ed94e884cab199d8048962
Chris Eager <chris@signal.org>
2024-04-17 17:45:56 -0500

    Update JsonMappingExceptionMapper “Early EOF” logic

--- service/src/main/java/org/whispersystems/textsecuregcm/mappers/JsonMappingExceptionMapper.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/mappers/JsonMappingExceptionMapper.java
@@ -3,15 +3,15 @@ package org.whispersystems.textsecuregcm.mappers;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
-import java.util.concurrent.TimeoutException;
 
 public class JsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
   @Override
   public Response toResponse(final JsonMappingException exception) {
-    if (exception.getCause() instanceof TimeoutException) {
+    if (exception.getCause() instanceof java.util.concurrent.TimeoutException) {
       return Response.status(Response.Status.REQUEST_TIMEOUT).build();
     }
-    if ("Early EOF".equals(exception.getMessage())) {
+    if (exception.getCause() instanceof org.eclipse.jetty.io.EofException
+        || exception.getMessage() != null && exception.getMessage().startsWith("Early EOF")) {
       // Some sort of timeout or broken connection
       return Response.status(Response.Status.BAD_REQUEST).build();
     }
