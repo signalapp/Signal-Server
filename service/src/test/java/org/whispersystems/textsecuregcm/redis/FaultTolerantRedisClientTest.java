876bf15a114aa61e7d78df2e1bac31cf2d690248
Jon Chambers <63609320+jon-signal@users.noreply.github.com>
2025-07-23 10:24:07 -0400

    Extend timeouts for Redis client tests

--- service/src/test/java/org/whispersystems/textsecuregcm/redis/FaultTolerantRedisClientTest.java
+++ service/src/test/java/org/whispersystems/textsecuregcm/redis/FaultTolerantRedisClientTest.java
@@ -1,25 +1,23 @@
 package org.whispersystems.textsecuregcm.redis;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.lettuce.core.RedisCommandTimeoutException;
 import io.lettuce.core.RedisException;
 import io.lettuce.core.resource.ClientResources;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import javax.annotation.Nullable;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.whispersystems.textsecuregcm.configuration.CircuitBreakerConfiguration;
 import org.whispersystems.textsecuregcm.configuration.RetryConfiguration;
-
-import javax.annotation.Nullable;
-import java.time.Duration;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // ThreadMode.SEPARATE_THREAD protects against hangs in the remote Redis calls, as this mode allows the test code to be
 // preempted by the timeout check
@@ -60,13 +58,13 @@ class FaultTolerantRedisClientTest {
     faultTolerantRedisClient = buildRedisClient(null, ClientResources.builder());
 
     final ExecutionException asyncException = assertThrows(ExecutionException.class,
-        () -> faultTolerantRedisClient.withConnection(connection -> connection.async().blpop(2 * TIMEOUT.toMillis() / 1000d, "key"))
+        () -> faultTolerantRedisClient.withConnection(connection -> connection.async().blpop(10 * TIMEOUT.toMillis() / 1000d, "key"))
             .get());
 
     assertInstanceOf(RedisCommandTimeoutException.class, asyncException.getCause());
 
     assertThrows(RedisCommandTimeoutException.class,
-        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(2 * TIMEOUT.toMillis() / 1000d, "key")));
+        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(10 * TIMEOUT.toMillis() / 1000d, "key")));
   }
 
   @Test
@@ -87,11 +85,11 @@ class FaultTolerantRedisClientTest {
 
     // the first call should time out and open the breaker
     assertThrows(RedisCommandTimeoutException.class,
-        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(2 * TIMEOUT.toMillis() / 1000d, key)));
+        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(10 * TIMEOUT.toMillis() / 1000d, key)));
 
     // the second call gets blocked by the breaker
     final RedisException e = assertThrows(RedisException.class,
-        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(2 * TIMEOUT.toMillis() / 1000d, key)));
+        () -> faultTolerantRedisClient.withConnection(connection -> connection.sync().blpop(10 * TIMEOUT.toMillis() / 1000d, key)));
     assertInstanceOf(CallNotPermittedException.class, e.getCause());
 
     // wait for breaker to be half-open
