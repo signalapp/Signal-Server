6fa01a5d48469aba0ddd2d0bf486b1023bd4ce5e
Jon Chambers <jon@signal.org>
2025-08-01 16:53:50 -0400

    Remove unused Lua scripts

--- service/src/main/resources/lua/account_database_crawler/unlock.lua
+++
@@ -1,8 +0,0 @@
--- keys: lock_key
--- argv: lock_value
-
-if redis.call("GET", KEYS[1]) == ARGV[1] then
-    return redis.call("DEL", KEYS[1])
-else
-    return 0
-end
--- service/src/main/resources/lua/renew_presence.lua
+++
@@ -1,7 +0,0 @@
-local presenceKey  = KEYS[1]
-local presenceUuid = ARGV[1]
-local expireSeconds = ARGV[2]
-
-if redis.call("GET", presenceKey) == presenceUuid then
-    redis.call("EXPIRE", presenceKey, expireSeconds)
-end
