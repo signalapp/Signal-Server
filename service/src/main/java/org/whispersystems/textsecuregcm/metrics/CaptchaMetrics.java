b702d9d5a94c9bc283c1795130aa43037eca797f
Jon Chambers <jon@signal.org>
2026-04-15 17:21:30 -0400

    Remove service level objectives for captcha outcome distribution

--- service/src/main/java/org/whispersystems/textsecuregcm/metrics/CaptchaMetrics.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/metrics/CaptchaMetrics.java
@@ -25,7 +25,6 @@ public class CaptchaMetrics {
             "context", context)
         .minimumExpectedValue(1.0d)
         .maximumExpectedValue(100.0d)
-        .serviceLevelObjectives(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
         .register(Metrics.globalRegistry)
         .record(Math.max(1, score));
   }
