a409b8e044a586c8094be89b62c7c575d0755189
Jon Chambers <63609320+jon-signal@users.noreply.github.com>
2026-09-22 14:20:30 -0400

    Revert "Disable flaky `ClearOrphanedFoundationDbQueuesCommandTest#aciReusedAf…"
    This reverts commit acbf1d828cfeac3c3a36e30897ca64ca4d593380.

--- service/src/test/java/org/whispersystems/textsecuregcm/workers/ClearOrphanedFoundationDbQueuesCommandTest.java
+++ service/src/test/java/org/whispersystems/textsecuregcm/workers/ClearOrphanedFoundationDbQueuesCommandTest.java
@@ -31,7 +31,6 @@ import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.whispersystems.textsecuregcm.entities.MessageProtos;
@@ -146,7 +145,6 @@ class ClearOrphanedFoundationDbQueuesCommandTest {
   }
 
   @Test
-  @Disabled("flaky test")
   void aciReusedAfterExistenceCheck() {
     final AciServiceIdentifier aci = new AciServiceIdentifier(UUID.randomUUID());
     foundationDbMessageStore.insert(aci, Map.of(Device.PRIMARY_ID, generateRandomMessage())).join();
